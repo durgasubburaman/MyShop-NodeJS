b68c9492d8874abef322c974b05789e6a57d39c3
Julien Corioland <julien.corioland@live.fr>
2016-10-20 22:17:17 +0200

    update server.js

--- src/ShopFront/server.js
+++ src/ShopFront/server.js
@@ -7,8 +7,10 @@ var path = require('path');
 var indexPath = path.join(__dirname + '/index.html');
 var port = process.env.PORT || 5000;   
 
+// when user GET / return the index.html file
 app.get('/', function(req, res) {
     res.sendFile(indexPath);
 });
 
+// start the app
 app.listen(port);
