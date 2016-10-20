7368606f501ccd11c4730362a68aacac76275750
Julien Corioland <julien.corioland@live.fr>
2016-10-20 16:22:56 +0200

    add CORS support in web APIs

--- src/RatingsApi/server.js
+++ src/RatingsApi/server.js
@@ -14,6 +14,13 @@ var os = require('os');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// add CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var port = process.env.PORT || 5002;        // set our port
 
 // ROUTES FOR OUR API
--- src/RecommandationsApi/server.js
+++ src/RecommandationsApi/server.js
@@ -16,6 +16,13 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 5003;        // set our port
 
+// add CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
