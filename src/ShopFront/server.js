c3b0864ed4c1b55fe83d15e2a88a8f91421634bb
Julien Corioland <julien.corioland@live.fr>
2016-10-20 22:13:20 +0200

    fix issue with port in server.js

--- src/ShopFront/server.js
+++ src/ShopFront/server.js
@@ -5,9 +5,10 @@ var app = express();
 var path = require('path');
 
 var indexPath = path.join(__dirname + '/index.html');
+var port = process.env.PORT || 5000;   
 
 app.get('/', function(req, res) {
     res.sendFile(indexPath);
 });
 
-app.listen(5000);
+app.listen(port);
