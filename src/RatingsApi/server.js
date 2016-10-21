92d31ec50f4e805dc2af529cd6223af42e52e92a
Julien Corioland <julien.corioland@live.fr>
2016-10-21 11:14:51 +0200

    update ratings server

--- src/RatingsApi/server.js
+++ src/RatingsApi/server.js
@@ -2,12 +2,12 @@
 
 var express = require('express');
 var app = express();
-var os = require('os');
+//var os = require('os');
 
 var port = process.env.PORT || 5002;   
 
 app.get('/', function(req, res) {
-    res.json('Executing RatingsApi version 1.0 - Host name : ' + os.hostname());
+    res.json('Executing RatingsApi version 1.0 - Host name : ');
 });
 
 // start the app
